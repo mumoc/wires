600dd129f64fe2b3c7f2202356d1ebc3826c03fb
Mumo Carlos <mumo.crls@gmail.com>
2015-09-01 10:25:46 -0500

    Config Heroku

--- config.rb
+++ config.rb
@@ -5,4 +5,12 @@ set :haml, { :ugly => true, :format => :html5 }
 
 configure :build do
   activate :relative_assets
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :gzip
+
+  activate :minify_html do |html|
+    html.remove_http_protocol = false
+  end
 end
